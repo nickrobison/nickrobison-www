9a4e85eb9a6aa3b7d6a879b260c5340e58912e27
Nick Robison <nick@nickrobison.com>
2018-05-09 10:10:34 -0700

    Parser now outputs valid yaml into posts.yml

--- tools/parser.ml
+++ tools/parser.ml
@@ -1,5 +1,24 @@
 open Core
 
+(** Creates the directory, prints a warning if the directory already exists.
+    * This doesn't handle any special cases for not being able to create dir.
+*)
+let create_dir directory =
+  try
+    Unix.mkdir directory
+  with
+    Unix.Unix_error(_,_,_) -> print_endline "Cannot create directory"
+
+(** Since YAMl doesn't allow for tabs as indentation, we use double spaces
+    * This function simply adds double spaces for the appropriate indentation level.
+    * Which avoids having empty '  ' strings everywhere.
+*)
+let add_padding pad_amount =
+  let rec aux n acc =
+    if n = 0 then acc else aux (n-1) ("  " ^ acc) in
+  aux pad_amount ""
+
+(** Filters out anything that isn't a markdown file. *)
 let filter_extension file =
   let extension = Filename.split_extension file in
   match (snd extension) with
@@ -8,6 +27,9 @@ let filter_extension file =
     | "md" -> true
     | _ -> false
 
+(** Process markdown file to extract YAML header and filtered markdown.
+    * This function writes the markdown to the [out_dir] and returns the YAML as a list.
+*)
 let process_file in_dir file out_dir =
   print_endline ("Processing file: " ^ file);
   let yaml_lines = ref [] in
@@ -19,7 +41,7 @@ let process_file in_dir file out_dir =
   protect ~f:(fun () ->
       try
         while true; do
-          let line = input_line file_chan in
+          let line = In_channel.input_line_exn file_chan in
         if (String.is_prefix line "---") then begin
           in_comment := (not !in_comment);
         end
@@ -29,24 +51,33 @@ let process_file in_dir file out_dir =
         done
       with End_of_file ->
         print_endline "Done with file";
-        print_endline (String.concat ?sep:(Some "\n") (List.rev !yaml_lines));
     )
     ~finally:(fun () -> In_channel.close file_chan);
-  (** Now, write the yaml*)
-  let output_file = Filename.concat out_dir (Filename.chop_extension file) ^ ".yml" in
-  Out_channel.write_all output_file ~data:(String.concat ?sep:(Some "\n") (List.rev !yaml_lines))
+  (** Now, write the md and return the yaml*)
+  let output_file = Filename.concat out_dir file in
+  Out_channel.write_all output_file ~data:(String.concat ?sep:(Some "\n") (List.rev !md_lines));
+  (** We need to append the '-' to the head of the first element in the list*)
+  let yaml_ordered = List.rev !yaml_lines in
+  ( "-\n" ^ (add_padding 2) ^ (List.hd_exn yaml_ordered)) :: (List.tl_exn yaml_ordered |>
+                                                              List.map ~f:(fun f -> (add_padding 1) ^ f))
 
-
+(** Run parser over input directory. *)
 let parse input_dir output_dir () =
   print_endline ("Parsing: " ^ input_dir);
-  try
-    Unix.mkdir output_dir;
-  with
-    Unix.Unix_error(_,_,_) -> print_endline "Cannot create directory";
+  create_dir output_dir;
+  let yaml_init =
+    ["posts:"]
+    in
   (** Get a list of all the files in the directory*)
   Sys.readdir input_dir |>
   Array.filter ~f:filter_extension |>
-  Array.iter ~f:(fun f -> process_file input_dir f output_dir)
+  Array.fold ~init:yaml_init ~f:(fun acc f ->
+      let yaml = process_file input_dir f output_dir in
+      acc @ yaml) |>
+  (fun yaml_lines ->
+     let yaml_file = Filename.concat output_dir "posts.yml" in
+     Out_channel.write_all yaml_file ~data:(String.concat ?sep:(Some ("\n" ^ (add_padding 1))) yaml_lines)
+  )
 
 
 let command =
