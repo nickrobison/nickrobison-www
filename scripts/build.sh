f5137a547c7899dd9d90df5f055aea7180edb849
Nick Robison <nick@nickrobison.com>
2018-05-13 23:30:19 -0700

    Set e in bash scripts.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 pushd src
-mirage configure -t ${MIRAGE_IMAGE} --dhcp=true --http-port=80 --host=${HOSTNAME}
+mirage configure -t ${MIRAGE_IMAGE} --dhcp=true --http-port=80 --host=${HOSTNAME} --build-id=${BUILD_ID}
 make depends
 make -j2
 popd
--- scripts/env.sh
+++ scripts/env.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# Mirage settings
 export MIRAGE_IMAGE=virtio
 export HOSTNAME=blog.nickrobison.com
+export BUILD_ID=$(date +%s)
+
+# Google Cloud settings
+export GAE_PYTHONPATH=${HOME}/.cache/google_appengine PATH=$PATH:${HOME}/google-cloud-sdk/bin PYTHONPATH=${PYTHONPATH}:${GAE_PYTHONPATH} CLOUDSDK_CORE_DISABLE_PROMPTS=1
+export GCLOUD_PROJECT=blog
+export GS_BUCKET=nickrobison-blog-images
+export IMAGE_FAMILY=mirage-blog
+export PUBLIC_IP=35.230.13.173
+export INSTANCE_NAME=blog
