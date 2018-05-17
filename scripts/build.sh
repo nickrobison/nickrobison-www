3dd07bf28f2aee40cc69c4f0f575e9a52b370270
Nick Robison <nick@nickrobison.com>
2018-05-17 15:04:38 -0700

    More build fixes

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# We currently need the dev version of yaml and our own cowabloga
+opam pin add -y cowabloga https://github.com/nickrobison/cowabloga.git
+#opam pin add -y --dev yaml
+
 pushd src
 mirage configure -t ${MIRAGE_IMAGE} --dhcp=true --http-port=80 --host=${HOSTNAME} --build-id=${BUILD_ID}
 make depends
