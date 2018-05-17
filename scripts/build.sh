079c333e45e31bce917fa87da978455b588b1668
Nick Robison <nick@nickrobison.com>
2018-05-17 16:02:16 -0700

    Remove yaml pin

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,11 @@ set -e
 
 # We currently need the dev version of yaml and our own cowabloga
 opam pin add -y cowabloga https://github.com/nickrobison/cowabloga.git
-opam pin add -y yaml https://github.com/avsm/ocaml-yaml#1dc0233449f93ccea2cc6272804551042b5e64b8
+#opam pin add -y yaml https://github.com/avsm/ocaml-yaml#1dc0233449f93ccea2cc6272804551042b5e64b8
 
 pushd src
 mirage configure -t ${MIRAGE_IMAGE} --dhcp=true --http-port=80 --host=${HOSTNAME} --build-id=${BUILD_ID}
 make depends
 make -j2
 popd
+.
