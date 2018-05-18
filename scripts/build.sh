75b848359666fd51ade614a01600d66279777779
Nick Robison <nick@nickrobison.com>
2018-05-18 15:56:47 -0700

    Setup deploy process.

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,4 +14,3 @@ mirage configure -t ${MIRAGE_IMAGE} --dhcp=true --http-port=80 --host=${HOSTNAME
 make depends
 make -j2
 popd
-.
--- scripts/env.sh
+++ scripts/env.sh
@@ -7,8 +7,9 @@ export BUILD_ID=$(date +%s)
 
 # Google Cloud settings
 export GAE_PYTHONPATH=${HOME}/.cache/google_appengine PATH=$PATH:${HOME}/google-cloud-sdk/bin PYTHONPATH=${PYTHONPATH}:${GAE_PYTHONPATH} CLOUDSDK_CORE_DISABLE_PROMPTS=1
-export GCLOUD_PROJECT=blog
+export GCLOUD_PROJECT=blog-203416
 export GS_BUCKET=nickrobison-blog-images
 export IMAGE_FAMILY=mirage-blog
 export PUBLIC_IP=35.230.13.173
 export INSTANCE_NAME=blog
+export GCE_ZONE=us-west1-c
