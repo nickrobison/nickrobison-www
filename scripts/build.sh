ddced7160588abe4a1fdd7be388ef455862badba
Nick Robison <nick@nickrobison.com>
2018-05-18 09:38:46 -0700

    Try xen again, which backs down to 4.05, and removes the shim pin.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ set -e
 
 # We currently need the dev version of yaml and our own cowabloga
 #opam pin add -y yaml https://github.com/avsm/ocaml-yaml#1dc0233449f93ccea2cc6272804551042b5e64b8
-opam pin add -y mirage-os-shim https://github.com/hannesm/mirage-os-shim.git#4.06
+#opam pin add -y mirage-os-shim https://github.com/hannesm/mirage-os-shim.git#4.06
 opam pin add -y cowabloga https://github.com/nickrobison/cowabloga.git
 opam install -y mirage
 
