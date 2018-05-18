f356e576a2c1fe31b39e4096dfbb8b194b4f2601
Nick Robison <nick@nickrobison.com>
2018-05-18 08:25:50 -0700

    Added missing -y to build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ set -e
 # We currently need the dev version of yaml and our own cowabloga
 #opam pin add -y cowabloga https://github.com/nickrobison/cowabloga.git
 #opam pin add -y yaml https://github.com/avsm/ocaml-yaml#1dc0233449f93ccea2cc6272804551042b5e64b8
-opam install mirage
+opam install -y mirage
 
 pushd src
 mirage configure -t ${MIRAGE_IMAGE} --dhcp=true --http-port=80 --host=${HOSTNAME} --build-id=${BUILD_ID}
