5922b033cb6575e892b82c8943afbdf731bc79c9
Nick Robison <nick@nickrobison.com>
2018-06-11 12:01:35 -0700

    Docker is now part of the deploy process.
    Bundle scripts are now sh. That way we can eventually move to alpine.

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 source scripts/env.sh
 
 solo5-mkimage disk.raw src/nickrobison_www.${MIRAGE_IMAGE}
-tar -zcvf mirage-${BUILD_ID}.tar.gz. disk.raw
+tar -zcvf mirage-${BUILD_ID}.tar.gz disk.raw
