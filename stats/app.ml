2a6c160b37f2e6b05fb64c98cd352824748095ec
Nick Robison <nick@nickrobison.com>
2019-04-21 22:22:06 -0400

    Stats JS is now failing to fetch data due to CORS.
    I think that means the HTTP/LWT stuff is working.
    I don't like missing Lwt and Async, but I'm not really sure how to get around it.

--- stats/app.ml
+++ stats/app.ml
@@ -7,6 +7,20 @@ module Model = struct
   type t = unit
 
   let cutoff _ _ = true
+
+  (**
+      XmlHttpRequest.get uri >>= fun frame ->
+      if frame.code == 200
+      then return frame.content
+      else print_endline "Fail"
+  *)
+
+
+  let fetch_data model =
+    let open Lwt.Infix in
+    let _ = Fetch.do_get ~uri:(Uri.make ~path:"/stats/ok" ()) >>= fun text -> print_endline text; Lwt.return_unit in
+    model
+
 end
 
 module State = struct
@@ -14,20 +28,27 @@ module State = struct
 end
 
 module Action = struct
-  type t = Nothing.t [@@deriving sexp_of]
+  type t = Refresh [@@deriving sexp_of]
 end
 
+let apply_action model action _ ~schedule_action:_ =
+  match (action: Action.t) with
+  | Refresh -> Model.fetch_data model
 
 let initial_model: Model.t =
   ()
 
-let on_startup ~schedule_action:_ _model =
+
+
+
+let on_startup ~schedule_action _model =
+  every (Time_ns.Span.of_sec 1.) (fun () -> schedule_action Action.Refresh);
   Deferred.unit
 
 let create model ~old_model:_ ~inject:_ =
   let open Incr.Let_syntax in
   let%map model = model in
-  let apply_action action _ ~schedule_action:_ = Nothing.unreachable_code action in
+  let apply_action = apply_action model in
   let view =
     let open Vdom in
     Node.div
---
+++ stats/fetch.ml
@@ -0,0 +1,9 @@
+open Lwt.Infix
+       open Js_of_ocaml_lwt
+
+let do_get ~uri =
+  let open XmlHttpRequest  in
+  let uri = Uri.to_string uri in
+  get uri >>= fun (frame) ->
+  print_endline frame.content;
+  Lwt.return frame.content
