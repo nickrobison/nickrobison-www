d51ceda8c135b1f508a8d804d52ad4772e10cc2f
Nick Robison <nickrobison@users.noreply.github.com>
2020-01-16 22:16:17 -0500

    Added Opam and HVT support (#48)
    * Initial opam support.
    
    Try to add an Opam file, in order to avoid some dependency issues down the road.
    
    * Updated compilation signatures.
    
    Everything is compiling and most of the warnings are silenced.
    
    * Silenced more warnings.
    
    I tried to remove the unused clock variables, but the signature chains fell
    apart.
    
    Not worthing looking at further.
    
    * Switch to hvt backend.
    
    I have no idea if this will work, but it's worth a shot.

--- src/cache.ml
+++ src/cache.ml
@@ -2,9 +2,9 @@ open Lwt.Infix
 
 module Make
     (S: Cohttp_lwt.S.Server)
-    (FS: Mirage_types_lwt.KV_RO)
-    (TMPL: Mirage_types_lwt.KV_RO)
-    (PClock: Mirage_types.PCLOCK)
+    (FS: Mirage_kv.RO)
+    (TMPL: Mirage_kv.RO)
+    (PClock: Mirage_clock.PCLOCK)
 = struct
   open Www_types
 
@@ -18,15 +18,13 @@ module Make
 
   type t = {
     table: (domain, value) Hashtbl.t;
-    clock: PClock.t;
     lifespan: Ptime.Span.t;
   }
 
-  let create clock duration =
+  let create duration =
     let table = Hashtbl.create 2 in
     {
       table = table;
-      clock = clock;
       lifespan = Ptime.Span.of_int_s (Duration.to_sec duration);
     }
 
@@ -36,7 +34,7 @@ module Make
     | None -> None
     | Some html ->
       Log.info (fun f -> f "Found in cache, expired?");
-      let now = PClock.now_d_ps cache.clock |> Ptime.v in
+      let now = PClock.now_d_ps () |> Ptime.v in
       if (Ptime.is_earlier now ~than:html.expires) then (Some html.html) else None
 
 
@@ -49,7 +47,7 @@ module Make
     | None ->
       Log.info (fun f -> f "Cache miss, building.");
       f loc tmpl >>= fun cow ->
-      let now = PClock.now_d_ps cache.clock |> Ptime.v in
+      let now = PClock.now_d_ps () |> Ptime.v in
       match (Ptime.add_span now cache.lifespan) with
       | Some s ->
         let value = {
